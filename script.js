6cdaaebb66ac97939a702acde39990fc905949f0
Corum Gold <goldcorum@gmail.com>
2022-04-20 12:23:40 -0400

    Add a blue H3

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ content.textContent = 'This is the glorious text-content!';
 container.appendChild(content);
 
 const redPara = document.createElement('p');
-redPara.classList.add('redPara');
+redPara.classList.add('red-para');
 redPara.textContent = "Hey I'm red!";
 redPara.style.color = "red";
 
 container.appendChild(redPara);
+
+const blueAychThree = document.createElement('h3');
+blueAychThree.classList.add('blue-aych-three');
+blueAychThree.textContent = "I'm a blue H3!";
+blueAychThree.style.color = "blue";
+
+container.appendChild(blueAychThree);
